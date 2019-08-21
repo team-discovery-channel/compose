f2a60ea1944e08c6f7ab063b3a9e0b0bbd502de9
LSmith-Zenoscave <lsmith@zenoscave.com>
2019-08-21 16:10:57 -0600

    source start
    Signed-off-by: LSmith-Zenoscave <lsmith@zenoscave.com>

---
+++ src/api/index.ts
@@ -0,0 +1,3 @@
+export const compose = (files: { [index: string]: string[] }) => {
+  return JSON.stringify(files);
+};
---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import path from 'path';
+import * as routes from './routes';
+
+dotenv.config();
+
+const port = process.env.SERVER_PORT;
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+routes.register(app);
+
+app.listen(port, () => {
+  console.log(`server listening on http://localhost:${port}/`);
+});
