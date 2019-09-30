bd0bd913f2d014844df180c544572f7b7c085b5d
Travis <tgolnoft@gmail.com>
2019-09-29 18:15:25 -0600

    linting

--- src/api/compose.python.ts
+++ src/api/compose.python.ts
@@ -4,21 +4,16 @@ import { Composable, Language } from './compose.language';
  * composes python source files
  */
 class Python extends Language {
-  constructor(){
-     super('python', ['.py', '__init__.py', '']) 
+  constructor() {
+    super('python', ['.py', '__init__.py', '']);
   }
-  getRegex(): RegExp[]{
-    return []
+  getRegex(): RegExp[] {
+    return [];
   }
-/**
-  * @param filelist ordered list of files returned by filterFiles. Last index is the entrypoint
-  * @param files the files from the zip
-  * @returns composedFile the combined file of
-  */
-	compose(filelist: string[], files: { [index: string]: string[] }): string {
-		return ""
-   }
-   
+
+  compose(filelist: string[], files: { [index: string]: string[] }): string {
+    return '';
+  }
 }
 
 export const python = new Python();
